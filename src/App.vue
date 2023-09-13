ca3be1f4ea538816acf953fcc812f500bd1516d9
Vishnu R <fsd4@riosis.com>
2023-09-13 15:54:03 +0530

    Tailwind css implementation

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ContainerComponent msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ContainerComponent from './components/ContainerComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ContainerComponent
   }
 }
 </script>
---
+++ src/components/ContainerComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="text-red-600 overline">Welcome Tailwind</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
